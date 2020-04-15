fcdaec0d2132d37e74742c346b760e7b866791c3
Nicole Samanich <nicolesamanich@gmail.com>
2020-04-15 00:02:31 -0400

    fix logo link

--- frontend/components/logo.jsx
+++ frontend/components/logo.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-
+import logo from './app/assets/logo3'; // gives image path
 
 const Logo = (props) => {
     return (<NavLink to="/" id='logolink'><span className='logoContainer'>
-                <img
-                    src="assets/logo3.png"
-                    className="logoImg" />
+        <img src={logo} alt="this is car image" />
                             UnlimitList
             </span></NavLink>
             )
